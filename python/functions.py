c9663b5921bd2ae61cb44e323e20503e628baaf4
Lucas Reges <lucas109080@gmail.com>
2026-02-16 19:53:19 -0300

    Finalizando tratamento de missing data e ajustando funções

--- python/functions.py
+++ python/functions.py
@@ -1,4 +1,5 @@
-
+﻿
+# -*- coding: utf-8 -*-
 import geopandas as gpd
 import seaborn as sns
 import matplotlib.pyplot as plt 
@@ -55,10 +56,54 @@ def comparativo_SQQ(win6,win5,win4):
     #preciso colocar a figura um pouco mais de espaço para o comentário abaixo
     fig.text(0.5, 0.01, "*As escalas variam drasticamente entre Sena e Quadra", 
          ha="center", fontsize=12, style='italic', bbox={'facecolor':'orange', 'alpha':0.2, 'pad':5})
-
-
     plt.show()
 
 
 
+
+
+    ############ Limpeza de strings ###################
+
+def limpar_strings(string):
+    ''' Esta função recebe uma string e:
+    - Coloca tudo em maiúsculo;
+    - Retira os acentos;
+    - Retira os espaços invisíveis antes e depois da string (mantém espaços internos)
+    retorna, por fim a string limpa 
+    '''
+    return (string.str.upper()
+         .str.normalize('NFKD')
+         .str.encode('ascii', errors='ignore')
+         .str.decode('utf-8')
+         .str.strip()
+         )
+    
+    
+
+############## Analisando completude do Dataframe ###############
+
+def vacancia(dataframe):
+    '''Função recebe um dataframe e devolve um mapa de calor de valores vazios no dataframe.'''
+    
+    plt.figure(figsize=(16, 8))
+    nulos = dataframe.isnull().sum() #isso aqui é uma lista da soma dos nulos em cada coluna
+    ax = sns.heatmap(dataframe.isnull(), 
+                     yticklabels=False, 
+                     cbar=False, 
+                     cmap='Greys_r' #reverti pra gerar um efeito visual de "vazio" melhor para o usuário
+                                )
+    
+    for i in range(len(nulos)):
+        plt.text(x = i + 0.5,y=-1.5 , s = nulos[i], ha='center') 
+        #pra cada coluna de nulo, geramos um texto nas posições 'x' e 'y' com o 's' sendo o conteúdo
+
+    plt.title(f'Total de registros vazios no dataframe:{nulos.sum()}\n~{(dataframe.shape[1]/dataframe.shape[0]):.2f}% do total', pad=30) #esse pad dá um espacinho pro titulo não ficar colado no gráfico    
+    plt.xticks(rotation=45)
+    plt.tight_layout()
+    plt.figtext(0.5, 0.01, "*Os números no topo das colunas são seus respectivos valores nulos", 
+         ha="center", 
+         fontsize=12, 
+         style='italic', 
+         bbox={'facecolor':'orange', 'alpha':0.2, 'pad':5})
+    plt.show()
 
